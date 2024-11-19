a4a9dc1d7fb188ad21f884acc1e4d047130a81d7
josegalvis1654 <jgalvis19@udi.edu.co>
2024-11-19 16:52:45 -0500

    Añadir detalles
    Se añadió un filtro para los lotes

--- src/productos/productos.controller.ts
+++ src/productos/productos.controller.ts
@@ -15,6 +15,15 @@ export class ProductosController {
   @Get('lotes-vencen')
   async obtenerLotesVencen(): Promise<any[]> {
     const lotes = await this.productosService.obtenerLotesVencen();
-    return lotes;
+    // Obtener la fecha de hoy y calcular el límite de 8 días
+    const hoy = new Date();
+    const limite = new Date();
+    limite.setDate(hoy.getDate() + 8);
+    // Filtrar los lotes que tienen una fecha de caducidad dentro del rango
+    const lotesVencenEn8Dias = lotes.filter((lote) => {
+      const fechaCaducidad = new Date(lote.fechacaducidad);
+      return fechaCaducidad >= hoy && fechaCaducidad <= limite;
+    });
+    return lotesVencenEn8Dias;
   }
 }
